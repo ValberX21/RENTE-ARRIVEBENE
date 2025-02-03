ca8cbca76135da6c2a32bbf69c1085018ea9b158
Valber Martins <martins_shinigami@hotmail.com>
2025-02-03 17:34:13 -0300

    adjust to debug application

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,22 @@
 const mongose = require("mongoose");
 
 const connDB = async () => {
+
     try
-    {
-        await mongose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+    {  
+        const uri = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/rentalDB'; 
+        
+        if (!uri) {
+          console.error("Mongo URI is missing!");
+          return;
+        }
+
+        // await mongose.connect(process.env.MONGO_URI,{
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        // });
+        await mongose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
         console.log('MongoDb connected...')
     } 
     catch (error)
