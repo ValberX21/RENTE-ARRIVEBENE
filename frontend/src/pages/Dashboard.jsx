b920bcb8e396328c5b2dd8f96a605bb8e4199700
Valber Martins <martins_shinigami@hotmail.com>
2025-02-07 14:21:24 -0300

    Begin work parts of dashboard (manager)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { NavLink} from 'react-router-dom';
+import '../Styles/Dashboard.css';
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard Page</h1>
-      <p>You're logged in!</p>
+      <header className="App-header"> {/* Apply the fixed header styling */}
+        <h3 className="d-flex justify-content-center m-3">
+          Rent estate Arrive
+        </h3>
+        <nav className="navv navbar navbar-expand-sm">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary">
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="App-body"> {/* Add this for additional content */}
+        {/* Additional content for your page goes here */}
+      </div>
     </div>
   );
 };
